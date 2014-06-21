e09bcef75f7415cf6af9c3cde949f5a1204f7349
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-06-21 13:37:37 +0000

    import package config.
    Conflicts:
    
    	.gitignore

--- app/controllers/PaypalController.php
+++ app/controllers/PaypalController.php
@@ -4,6 +4,34 @@ class PaypalController extends BaseController
 {
 	public function prepareExpressCheckout()
 	{
-		// TODO
+        $storage = $this->getPayum()->getStorage('Payum\Core\Model\ArrayObject');
+
+        $details = $storage->createModel();
+        $details['PAYMENTREQUEST_0_CURRENCYCODE'] = 'EUR';
+        $details['PAYMENTREQUEST_0_AMT'] = 1.23;
+        $storage->updateModel($details);
+
+        $captureToken = $this->getTokenFactory()->createCaptureToken('paypal_es', $details, 'payment_done');
+        $details['RETURNURL'] = $captureToken->getTargetUrl();
+        $details['CANCELURL'] = $captureToken->getTargetUrl();
+        $storage->updateModel($details);
+
+        return \Redirect::to($captureToken->getTargetUrl());
 	}
+
+    /**
+     * @return \Payum\Core\Registry\RegistryInterface
+     */
+    protected function getPayum()
+    {
+        return \App::make('payum');
+    }
+
+    /**
+     * @return \Payum\Core\Security\GenericTokenFactoryInterface
+     */
+    protected function getTokenFactory()
+    {
+        return \App::make('payum.security.token_factory');
+    }
 }
